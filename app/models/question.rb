caf42794c13df0dd5a13b77eeda22af5eb9d44cb
Madhu Ramalingam <ramalingam.madhu@gmail.com>
2015-05-20 21:18:21 +1000

    create models and wire up the models, views and controllers to make it functional

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   has_many :answers
+  include HasGravatar
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/about' => 'home#about'
-  post '/questions' => 'home#temp'
-  get '/questions/:id' => 'home#question'
-  post '/answers' => 'home#temp'
+  post '/questions' => 'questions#create'
+  get '/questions/:id' => 'questions#show', :as => :question
+  post '/answers' => 'answers#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
