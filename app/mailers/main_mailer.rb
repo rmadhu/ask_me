f8e336114850060e8537cc88ede3a1c2ad07e1f8
Madhu Ramalingam <ramalingam.madhu@gmail.com>
2015-05-21 12:48:13 +1000

    generate action mailer functionality..

---
+++ app/mailers/main_mailer.rb
@@ -0,0 +1,14 @@
+class MainMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.main_mailer.notify_question_author.subject
+  #
+  def notify_question_author
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
---
+++ test/mailers/main_mailer_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class MainMailerTest < ActionMailer::TestCase
+  test "notify_question_author" do
+    mail = MainMailer.notify_question_author
+    assert_equal "New answer to your question", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+end
